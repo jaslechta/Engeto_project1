c103ea0ed7cfbbc023b1b0a7dfacec0473b984a4
Jan Slechta <janslechta31@gmail.com>
2023-07-11 12:18:28 +0200

    hlavicka

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world!!!")
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Jan Šlechta
+email: janslechta31@gmail.com
+discord: honzas0100
+"""
+import ...
